0371ea875ebde3b9be81f69e42eea3b167314c32
Sferratore <sferratore.francescopaolo00@gmail.com>
2024-01-04 11:43:17 +0100

    Added comments on DishesController.cs

--- ShizukaSushi/Controllers/DishesController.cs
+++ ShizukaSushi/Controllers/DishesController.cs
@@ -6,13 +6,17 @@ using ShizukaSushi.DatabaseContext;
 using ShizukaSushi.Models;
 
 
-[ApiController]
-public class DishesController : ControllerBase
+/* DishesController is the main API controller 
+ * of the application */
+
+
+[ApiController] //Attribute used to define DishesController class as API Controller.
+public class DishesController : ControllerBase //Inheritance of ControllerBase defines the class as controller, which gets automatically managed by DI defined on Program.cs.
 {
-    private readonly ShizukaDbContext _context;
+    private readonly ShizukaDbContext _context; //Contains the DbContext in use.
 
 
-
+    //Constructor. Implements DI of ShizukaDbContext.
     public DishesController(ShizukaDbContext context)
     {
         _context = context;
@@ -20,8 +24,8 @@ public class DishesController : ControllerBase
 
 
 
-    [HttpPost("/dishes")]
-    public IActionResult CreateDish(CreateDishRequest request)
+    [HttpPost("/dishes")] //Attribute that defines method call. In this case, you should call */dishes with a POST call.
+    public IActionResult CreateDish(CreateDishRequest request) //Method that handles Dish creation.
     {
         try
         {
@@ -40,24 +44,28 @@ public class DishesController : ControllerBase
         return Ok(request);
     }
 
+
     [HttpGet("/dishes/{id:guid}")]
-    public IActionResult GetDish(Guid id)
+    public IActionResult GetDish(Guid id) //Method that handles Dish data retrieval.
     {
         Dish? d = _context.Dishes.Where(x => x.Id.Equals(id)).FirstOrDefault();
         return Ok(d);
     }
 
+
     [HttpPut("/dishes/{id:guid}")]
-    public IActionResult UpdateDish(Guid id, UpdateDishRequest request)
+    public IActionResult UpdateDish(Guid id, UpdateDishRequest request) //Method that handles Dish data update.
     {
         try
         {
+            //Delete Dish
             Dish? d1 = _context.Dishes.Where(x => x.Id.Equals(id)).FirstOrDefault();
             if (d1 != null)
             {
                 _context.Dishes.Remove(d1);
             }
 
+            //Create new one with request info and same id.
             Dish d2 = new Dish(id, request.Name, request.Description, DateTime.UtcNow, request.Ingredients);
             _context.Dishes.Add(d2);
             _context.SaveChanges();
@@ -73,8 +81,9 @@ public class DishesController : ControllerBase
         return Ok(request);
     }
 
+
     [HttpDelete("/dishes/{id:guid}")]
-    public IActionResult DeleteDish(Guid id)
+    public IActionResult DeleteDish(Guid id) //Method that handles Dish deletion.
     {
         Dish? d1 = _context.Dishes.Where(x => x.Id.Equals(id)).FirstOrDefault();
         if (d1 != null)
