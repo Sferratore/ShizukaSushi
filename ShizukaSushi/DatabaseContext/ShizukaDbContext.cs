af100066d878fae71a863d844eb3d0d04978b419
Sferratore <sferratore.francescopaolo00@gmail.com>
2024-01-04 11:53:20 +0100

    Commented ShizukaDbContext.cs

--- ShizukaSushi/DatabaseContext/ShizukaDbContext.cs
+++ ShizukaSushi/DatabaseContext/ShizukaDbContext.cs
@@ -1,25 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using ShizukaSushi.Models;
 
-namespace ShizukaSushi.DatabaseContext
+namespace ShizukaSushi.DatabaseContext;
+
+/* ShizukaDbContext is the main 
+ * Dbcontext of the application.*/
+
+public class ShizukaDbContext : DbContext //Inheritance of DbContext defines ShizukaDbContext as a DbContext. DbContext are initialized with DI in Program.cs.
 {
-    public class ShizukaDbContext : DbContext
+    public DbSet<Dish> Dishes { get; set; } //Dishes is the main entity of the application.
+    public ShizukaDbContext(DbContextOptions<ShizukaDbContext> options) : base(options) //Constructor defines that the options of the DbContext are inherited by DI definition in Program.cs.
     {
-        public DbSet<Dish> Dishes { get; set; }
-        public ShizukaDbContext(DbContextOptions<ShizukaDbContext> options) : base(options)
-        {
-        }
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //Defines that "Ingredients" list of strings will be converted in a single value of them concatenated. Does not referentiate entity.
-            modelBuilder.Entity<Dish>()
-                .Property(e => e.Ingredients)
-                .HasConversion(
-                    v => string.Join(";", v),
-                    v => v.Split(';', StringSplitOptions.RemoveEmptyEntries).ToList());
-        }
-
     }
+
+    /* OnModelCreating defines action to take when a Db operation (migration, saving new data...) is taking place. */
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Defines that "Ingredients" list of strings will be treated as a single value of them concatenated. Does not referentiate a different entity.
+        modelBuilder.Entity<Dish>()
+            .Property(e => e.Ingredients)
+            .HasConversion(
+                v => string.Join(";", v),
+                v => v.Split(';', StringSplitOptions.RemoveEmptyEntries).ToList());
+    }
+
 }
